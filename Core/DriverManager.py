4d9fa9e26b4778af6823f412b6d05774cea676c8
Hitesh Rajput <84222265+hsrajput311621@users.noreply.github.com>
2026-03-28 10:22:30 +0530

    DriverManager.py

--- Core/DriverManager.py
+++ Core/DriverManager.py
@@ -49,22 +49,32 @@ class DriverManager:
         # --------------------------------------------------------
         # Apply browser options from config.json
         # --------------------------------------------------------
+        chrome_options = Options()
+        chrome_options.add_argument("--incognito")
+        chrome_options.add_argument("--disable-popup-blocking")
+        chrome_options.add_argument("--disable-notifications")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--headless=new")
 
-        if self.config.get("headless"):
-            chrome_options.add_argument("--headless=new")  # modern headless
+        # if self.config.get("headless"):
+        #     chrome_options.add_argument("--headless=new")  # modern headless
 
-        if self.config.get("incognito"):
-            chrome_options.add_argument("--incognito")
+        # if self.config.get("incognito"):
+        #     chrome_options.add_argument("--incognito")
 
-        if self.config.get("disable_notifications"):
-            chrome_options.add_argument("--disable-notifications")
+        # if self.config.get("disable_notifications"):
+        #     chrome_options.add_argument("--disable-notifications")
 
-        if self.config.get("start_maximized"):
-            chrome_options.add_argument("--start-maximized")
+        # if self.config.get("start_maximized"):
+        #     chrome_options.add_argument("--start-maximized")
 
-        # Remove automation flags
-        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        chrome_options.add_experimental_option("useAutomationExtension", False)
+        # # Remove automation flags
+        # chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # chrome_options.add_experimental_option("useAutomationExtension", False)
 
         # --------------------------------------------------------
         # Start Chrome using webdriver_manager (auto-download)
